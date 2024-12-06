9a09d3318ff334e8f2dbdbd22f6f54dc21892da1
Christopher Borum <cborum@iwcs.k12.va.us>
2024-12-06 19:32:21 +0000

    Initial digital hall pass setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
